6f2b3c6dc1bbeae98de7ec51feb05f0b497ea626
Akhil Kannan <akhilork@gmail.com>
2023-10-12 21:42:17 +0530

    Configured to import svg

---
+++ src/components/loading.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import AppLogo from '../assets/svg/app-logo.svg';
+import {Colors} from '../styles';
+
+const Loading = (): JSX.Element => {
+  return (
+    <View style={styles.loadingConatiner}>
+      <AppLogo height={86} width={260} color={Colors.white} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  loadingConatiner: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.primary,
+  },
+});
+
+export default Loading;
