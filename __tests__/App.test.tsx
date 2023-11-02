0b74319bb349a98d1e9236b716e4a2c8be008548
Akhil Kannan <akhilork@gmail.com>
2023-11-02 22:23:46 +0530

    Fixed jest test issues

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -7,11 +7,15 @@ import React from 'react';
 import App from '../App';
 
 // Note: import explicitly to use the types shiped with jest.
-import {it} from '@jest/globals';
+import {jest, it} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer, {act} from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+jest.useFakeTimers();
+
+it('renders correctly', async () => {
+  await act(async () => {
+    renderer.create(<App />);
+  });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['./jestSetup.js'],
+  moduleNameMapper: {
+    '\\.svg': '<rootDir>/__mocks__/svgMock.js',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?(@react-native|react-native)|react-navigation|@react-navigation/.*)',
+  ],
 };
