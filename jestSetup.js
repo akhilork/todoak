3df3226ab042adc87260205b26026c07a896ffd3
Akhil Kannan <akhilork@gmail.com>
2023-11-03 22:17:27 +0530

    Fixed the redux-persist jest error

--- jestSetup.js
+++ jestSetup.js
@@ -3,3 +3,13 @@ import {jest} from '@jest/globals';
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
 );
+
+jest.mock('redux-persist', () => {
+  const real = jest.requireActual('redux-persist');
+  return {
+    ...real,
+    persistReducer: jest
+      .fn()
+      .mockImplementation((config, reducers) => reducers),
+  };
+});
